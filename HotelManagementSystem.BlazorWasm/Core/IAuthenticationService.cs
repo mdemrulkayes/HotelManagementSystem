f52317cd23863999a3e63a8c773e1d4338991c89
Md. Emrul Kayes <mdemrul.kayes@bracits.com>
2020-10-13 21:54:48 +0600

    Initial Commit

---
+++ HotelManagementSystem.BlazorWasm/Core/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.DataModels;
+
+namespace HotelManagementSystem.BlazorWasm.Core
+{
+    public interface IAuthenticationService
+    {
+        Task<UserDTO> SignUp();
+    }
+}
---
+++ HotelManagementSystem.BlazorWasm/Service/AuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.DataModels;
+using HotelManagementSystem.BlazorWasm.Core;
+
+namespace HotelManagementSystem.BlazorWasm.Service
+{
+    public class AuthenticationService: IAuthenticationService
+    {
+        public async Task<UserDTO> SignUp()
+        {
+            //API Calling
+            throw new NotImplementedException();
+        }
+    }
+}
